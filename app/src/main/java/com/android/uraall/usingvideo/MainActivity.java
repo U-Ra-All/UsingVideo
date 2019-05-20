18686f05ceb855d59b1918eebd0c3a1b853f24c4
YouRa <allakhverdovyuriy@gmail.com>
2019-05-20 19:54:20 +0300

    Final

---
+++ app/src/main/java/com/android/uraall/usingvideo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.android.uraall.usingvideo;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String path="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        Uri uri=Uri.parse(path);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+    }
+}
